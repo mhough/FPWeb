4183bc215dc43196582cf1dbb5e9333121db9c89
Simon Forman <forman.simon@gmail.com>
2013-03-21 21:36:51 -0700

    Parameterized CSS (w/ SCSS in Python)
    Not taking full advantage of this yet, just trying it out.
    
    Haz parameterized minified CSS now, but feels clunky.  I'd like something
    like html.py but for CSS...

--- site_css.py
+++ site_css.py
@@ -1,6 +1,20 @@
+from scss import Scss
 
 
-site_default = '''\
+#: Define some CSS variables.
+params = dict(('$' + k, v) for k, v in dict(
+
+  body_font = "'Oxygen', serif",
+  title_font = "'Cutive', sans-serif",
+
+  foreground = '#333333',
+
+  padding = '2em',
+
+  ).iteritems())
+
+
+rules = '''\
 /* from Foundation CSS */
 input[type="text"], input[type="password"], input[type="date"], input[type="datetime"], input[type="email"], input[type="number"], input[type="search"], input[type="tel"], input[type="time"], input[type="url"], textarea { background-color: white; font-family: inherit; border: 1px solid #cccccc; -webkit-border-radius: 2px; -moz-border-radius: 2px; -ms-border-radius: 2px; -o-border-radius: 2px; border-radius: 2px; -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1); -moz-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1); box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1); color: rgba(0, 0, 0, 0.75); display: block; font-size: 14px; margin: 0 0 12px 0; padding: 6px; height: 32px; width: 100%; -webkit-transition: all 0.15s linear; -moz-transition: all 0.15s linear; -o-transition: all 0.15s linear; transition: all 0.15s linear; }
 input[type="text"].oversize, input[type="password"].oversize, input[type="date"].oversize, input[type="datetime"].oversize, input[type="email"].oversize, input[type="number"].oversize, input[type="search"].oversize, input[type="tel"].oversize, input[type="time"].oversize, input[type="url"].oversize, textarea.oversize { font-size: 17px; padding: 4px 6px; }
@@ -14,16 +28,15 @@ select { width: 100%; }
 @import url(http://fonts.googleapis.com/css?family=Cutive|Oxygen:400,700);
 
 body {
-    line-height: 1.5;
-    font-family: 'Oxygen', serif;
-    color: #333333;
+  line-height: 1.5;
+  font-family: $body_font;
+  color: $foreground;
 }
 
 h1, h2, h3, h4, h5, h6, .heavy {
-	font-family: 'Cutive', sans-serif;
-	font-weight: bold;
+  font-family: $title_font;
+  font-weight: bold;
 }
-
 
 #container {
   border: 1px dashed gray;
@@ -61,3 +74,10 @@ form {
   margin: 3px;
 }
 '''
+
+
+site_default = Scss(scss_vars=params).compile(rules)
+
+
+if __name__ == '__main__':
+  print site_default
