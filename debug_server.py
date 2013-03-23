80bcdc81a3eedcba19d7e7d5c7484b7150499447
Simon Forman <forman.simon@gmail.com>
2013-03-22 22:38:26 -0700

    Use the werkzeug dev server if available.

--- debug_server.py
+++ debug_server.py
@@ -1,7 +1,16 @@
-from wsgiref.simple_server import make_server
+#!/usr/bin/env python
 from main import app
 
 
+HOST, PORT = 'localhost', 5000
+
+
 if __name__ == '__main__':
-  print "Serving on http://localhost:5000/ ..."
-  make_server('', 5000, app).serve_forever()
+  print "Serving on http://%s:%i/ ..." % (HOST, PORT)
+  try:
+    from werkzeug.serving import run_simple
+  except ImportError:
+    from wsgiref.simple_server import make_server
+    make_server(HOST, PORT, app).serve_forever()
+  else:
+    run_simple(HOST, PORT, app, use_reloader=True)
